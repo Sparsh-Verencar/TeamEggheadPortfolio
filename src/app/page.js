3c2b9a7b53beb6668a910350cb5c5b68990c7eed
Sparsh <verencarsparsh@gmail.com>
2025-07-19 15:13:26 +0530

    added gmail functionality and changed folder color

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,14 @@
 "use client"
+import Gmail from "@/components/Gmail";
 import Folder from "@/components/react_bits/Components/Folder/Folder";
-import { Button } from "@/components/ui/button";
 import WindowTaskBar from "@/components/WindowTaskBar";
-import { motion } from "motion/react"
 
 export default function Home() {
   return (
     <div className="relative w-screen h-screen flex flex-col items-center justify-center bg-gray-950">
     
-      <Folder/>
+      <Folder size={1} color="#F3F708" className="custom-folder"/>
+      <Gmail/>
       <WindowTaskBar/>
     </div>
   );
