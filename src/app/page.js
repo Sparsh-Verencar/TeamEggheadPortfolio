7c627ef2251d3be33e0a79b7c6ef61e12ca00d3d
Sparsh <verencarsparsh@gmail.com>
2025-07-19 14:34:48 +0530

    folders and windowtaskbar created along with react bit folder

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,15 @@
 "use client"
+import Folder from "@/components/react_bits/Components/Folder/Folder";
 import { Button } from "@/components/ui/button";
+import WindowTaskBar from "@/components/WindowTaskBar";
 import { motion } from "motion/react"
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center">
-    <h1>Team Egghead</h1>
-    <Button>Click</Button>
-    <h1>Framer motion button below</h1>
-      <motion.button 
-        className="bg-amber-700"
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.95 }}
-        onHoverStart={() => console.log('hover started!')}
-      >hello</motion.button>
+    <div className="relative w-screen h-screen flex flex-col items-center justify-center bg-gray-950">
+    
+      <Folder/>
+      <WindowTaskBar/>
     </div>
   );
 }
